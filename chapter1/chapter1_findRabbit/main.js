529f37a3ed782304248db3321800c399cbacc7bf
wldns0622 <wldns12378@gmail.com>
2021-04-22 23:49:01 +0900

    토끼를 찾아라 게임
    버튼을 클릭하면 당근들 사이에서 토끼가 있는곳으로 스크롤링 되는 이벤트 작성

---
+++ chapter1/chapter1_findRabbit/main.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const rabbit = document.querySelector('#rabbit');
+
+button.addEventListener('click', () => {
+  rabbit.scrollIntoView({ behavior: 'smooth', block: 'center' });
+})
